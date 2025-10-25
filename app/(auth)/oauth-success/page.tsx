0320c02f181c76529b089e5bc44f717c55225811
Yotrages <qayyumoladimeji@gmail.com>
2025-10-25 17:54:59 +0100

    Oauth redirect

--- app/(auth)/oauth-success/page.tsx
+++ app/(auth)/oauth-success/page.tsx
@@ -1,45 +1,50 @@
-'use client'
-import { setUserCredentials } from '@/libs/redux/authSlice';
-import { AppDispatch } from '@/libs/redux/store';
-import { setCookie } from 'cookies-next';
-import { useRouter } from 'next/navigation';
-import { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+"use client";
+import { setUserCredentials } from "@/libs/redux/authSlice";
+import { AppDispatch } from "@/libs/redux/store";
+import { setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 
 const OAuthSuccess = () => {
   const router = useRouter();
   const hasRun = useRef(false);
-  const dispatch: AppDispatch = useDispatch()
+  const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    if (hasRun.current) return; 
+    if (hasRun.current) return;
     hasRun.current = true;
 
     const queryParams = new URLSearchParams(window.location.search);
-    const token = queryParams.get('token');
-    const name = queryParams.get('name');
-    const id = queryParams.get('id');
-    const email = queryParams.get('email');
-    const avatar = queryParams.get('avatar')
-    const from = queryParams.get('from')
+    const token = queryParams.get("token");
+    const name = queryParams.get("name");
+    const id = queryParams.get("id");
+    const email = queryParams.get("email");
+    const avatar = queryParams.get("avatar");
+    const from = queryParams.get("from");
 
-    console.log('Received:', { token, name, email });
+    console.log("Received:", { token, name, email });
 
     if (token && name && email && id && avatar) {
-      setCookie('auth-token', token);
-    dispatch(setUserCredentials({
-        user: {
+      setCookie("auth-token", token);
+      dispatch(
+        setUserCredentials({
+          user: {
             _id: id,
             email: email,
             username: name,
             online: true,
-            avatar: avatar
-        }
-    }))
-    
-    router.push(typeof from === 'string' && from !== '' ? from : "/")    } else {
-      console.error('OAuth data missing from URL');
-      router.push(`/login?error=${encodeURIComponent("OAuth data missing from URL")}`);
+            avatar: avatar,
+          },
+        })
+      );
+
+      router.push(from && from || '/');
+    } else {
+      console.error("OAuth data missing from URL");
+      router.push(
+        `/login?error=${encodeURIComponent("OAuth data missing from URL")}`
+      );
     }
   }, [router, dispatch]);
 
